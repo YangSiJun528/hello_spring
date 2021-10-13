54e3a5ac3ba7327e47b12c6c1cea08985cacd2e2
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-10-13 22:54:44 +0900

    Add Member Service

---
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,34 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    //final이 붙은 변수,메서드는 한 번 선언(초기화) 이후 변경할 수 없음
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+        // 이름 중복X
+        validateDuplicateMember(member); //중복 검사
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 회원입니다");
+            });
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
