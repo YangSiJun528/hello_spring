30c41467fef64e67650acf3ea57fb0a09bd48c5b
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-10-18 16:43:53 +0900

    Add 회원 웹 기능 - 조회

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -5,8 +5,11 @@ import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 /*
@@ -38,6 +41,12 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
 
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members",members);
+        return "members/memberList";
     }
 }
